5204b36e899181c6b407a3070991f5080ba3e52e
seojin36 <168956404+seojin36@users.noreply.github.com>
2024-09-14 14:08:42 +0900

    Update taxcal.js

--- taxcal.js
+++ taxcal.js
@@ -1,9 +1,9 @@
 function add() {
     let newdiv = document.createElement('p');
     let name = number.value + '번 ' + namek.value + " 세금: ";
-    let num = 55 + parseInt(estate.value) + parseInt(salary.value)/5;
-    let tax = name + num + '경 월급: ' + parseInt(salary.value) + '경 차액: ' + (parseInt(salary.value) * 1.5 - num) + '경';
-    newdiv.innerHTML = tax;
+    let tax = 55 + parseInt(estate.value) + parseInt(salary.value)/5;
+    let text = name + ' | 세금' + tax + '경 | 월급: ' + parseInt(salary.value) + '경 | 차액: ' + (parseInt(salary.value) * 1.5 - num) + '경';
+    newdiv.innerHTML = text;
     document.getElementById("list").appendChild(newdiv);
 }
   
